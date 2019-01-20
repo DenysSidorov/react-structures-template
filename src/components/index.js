caf5f06e6963c537724c6f7f7b42bcfb69333a55
Denys Sidorov <000scorpions000@gmail.com>
2019-01-20 19:02:38 +0200

    add mock data

--- src/components/index.js
+++ src/components/index.js
@@ -12,7 +12,24 @@ class ZiPCodeComponent extends React.Component {
     searchValue: '',
     searchError: '',
     isFetching: false,
-    zipCodeItems: [],
+    zipCodeItems: [
+      {
+        _id: 'JR55AMG86VXEI',
+        country: 'United States',
+        'country abbreviation': 'US',
+        places: [
+          {
+            latitude: '33.9731',
+            longitude: '-118.2479',
+            'place name': 'Los Angeles',
+            state: 'California',
+            'state abbreviation': 'CA',
+            'post code': '90001',
+          },
+        ],
+        'post code': '90001',
+      },
+    ],
   };
 
   // input handler
