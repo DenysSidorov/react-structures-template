c43b800bacc8d0e0d752c3863db40cab780e1022
Denys Sidorov <000scorpions000@gmail.com>
2019-01-26 00:39:59 +0200

    fix structure

--- src/components/ZipCodeItem.js
+++ src/components/ZipCodeItem.js
@@ -3,42 +3,39 @@ import PropTypes from 'prop-types';
 import {LangConsumer} from '../contexts/lang';
 // import langState from '../config/lang';
 
-const ZipCodeItem = ({el, currentItem, selectItem, removeItem, langState}) => {
-  console.log('');
-  return (
-    <div
-      className="zipCodeCont_body_list_item"
-      onClick={() => {
-        selectItem(el);
-      }}
-      style={{backgroundColor: el._id === currentItem._id ? '#3dce78' : ''}}
-    >
-      <div className="itemLeft">
-        <div className="zipCodeCont_body_list_item_logo">
-          <img
-            alt=""
-            src="/static-files/help-Circle.png"
-            className="zipCodeCont_body_list_item_logo_img"
-          />
-        </div>
-        <div className="zipCodeCont_body_list_item_text noWrap">
-          <span>
-            {el.places[0]['place name']},{el.places[0]['state abbreviation']}
-          </span>
-        </div>
+const ZipCodeItem = ({el, currentItem, selectItem, removeItem, langState}) => (
+  <div
+    className="zipCodeCont_body_list_item"
+    onClick={() => {
+      selectItem(el);
+    }}
+    style={{backgroundColor: el._id === currentItem._id ? '#3dce78' : ''}}
+  >
+    <div className="itemLeft">
+      <div className="zipCodeCont_body_list_item_logo">
+        <img
+          alt=""
+          src="/static-files/help-Circle.png"
+          className="zipCodeCont_body_list_item_logo_img"
+        />
       </div>
-
-      <div
-        className="itemRemove"
-        onClick={e => {
-          removeItem(e, el);
-        }}
-      >
-        {langState.translate('Remove')}
+      <div className="zipCodeCont_body_list_item_text noWrap">
+        <span>
+          {el.places[0]['place name']},{el.places[0]['state abbreviation']}
+        </span>
       </div>
     </div>
-  );
-};
+
+    <div
+      className="itemRemove"
+      onClick={e => {
+        removeItem(e, el);
+      }}
+    >
+      {langState.translate('Remove')}
+    </div>
+  </div>
+);
 
 const wrapper = props => (
   <LangConsumer>{langState => <ZipCodeItem langState={langState} {...props} />}</LangConsumer>
