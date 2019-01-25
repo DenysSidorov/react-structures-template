7c54b24d6ac41b8d6d949c131af97fb2b0db6fdd
Denys Sidorov <000scorpions000@gmail.com>
2019-01-25 17:06:55 +0200

    fast fix

--- src/components/LangFlags.js
+++ src/components/LangFlags.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LangFlags = () => (
   <div className="langContainer">
-    <img className="langContFags" src="/static-files/ru-flag.png" />
+    <img className="langContFags" src="/static-files/rus-flag.png" />
     <img className="langContFags" src="/static-files/uk-flag.png" />
   </div>
 );
