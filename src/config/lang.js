c10bf7c1811a436370f2fb15b75963c2f01b922c
Denys Sidorov <000scorpions000@gmail.com>
2019-01-26 00:35:57 +0200

    rewrite language object

--- src/config/lang.js
+++ src/config/lang.js
@@ -1,9 +1,9 @@
-const translations = Symbol('translations');
-const langArr = Symbol('langArr');
+// const translations = Symbol('translations');
+// const langArr = Symbol('langArr');
 // const currentLang = Symbol('currentLang');
 
-class LangState {
-  [translations] = {
+const langState = {
+  translations: {
     ru: {
       Remove: 'Удалить',
       dateTime: 'MMMM HH:mm',
@@ -20,24 +20,28 @@ class LangState {
       Done: 'Done',
       'Not Done': 'Not Done',
     },
-  };
+  },
 
-  [langArr] = ['en', 'ru'];
+  langArr: ['en', 'ru'],
 
-  currentLang = 'en';
+  currentLang: 'en',
 
-  translate = word => this[translations][this.getCurrentLang()][`${word}`];
+  translate(word) {
+    return this.translations[this.getCurrentLang()][`${word}`];
+  },
 
-  getCurrentLang = () => this.currentLang;
+  getCurrentLang() {
+    return this.currentLang;
+  },
 
-  setCurrentLang = lang => {
-    if (this[langArr].indexOf(lang) !== -1) {
+  setCurrentLang(lang) {
+    if (this.langArr.indexOf(lang) !== -1) {
       this.currentLang = lang;
     } else {
       throw Error("Can't find this language!");
     }
-  };
-}
+  },
+};
 
-const LangStateInstance = new LangState();
-export default LangStateInstance;
+// const LangStateInstance = new LangState();
+export default langState;
