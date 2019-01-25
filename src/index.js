48c45c9cd9a898178c3ed397361e74585bb4bcca
Denys Sidorov <000scorpions000@gmail.com>
2019-01-25 17:49:02 +0200

    prepare app for context

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import ReactDOM from 'react-dom';
 import App from './components/index';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+const container = document.getElementById('app');
+
+if (container) {
+  ReactDOM.render(<App />, container);
+}
+
+console.log(process.env.NODE_ENV, 'MODE IN REACT');
